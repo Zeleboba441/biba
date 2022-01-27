e0bb36c5d595d2fb313486b23cdf6bb9dac7755b
Zeleboba441 <89891909+Zeleboba441@users.noreply.github.com>
2022-01-27 15:24:14 +1000

    Add files via upload

---
+++ Lublicorp 2.0/Lublicorp 2.0/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lublicorp_2._0
+{
+     static class Data
+    {
+        public static ObservableCollection<Person> Persons = new ObservableCollection<Person>();
+        private static Page currentPage;
+        public static event EventHandler CurrentPageChanged;
+        public static Page CurrentPage
+        {
+            get => currentPage;
+            internal set
+            {
+                currentPage = value;
+                CurrentPageChanged?.Invoke(CurrentPage, new EventArgs());
+            }
+        }
+    }
+}
